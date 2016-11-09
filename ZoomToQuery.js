303dbd77d4b6e4c8fcf31d0f47451871e9973c97
Javier Escudero <javierparkour@gmail.com>
2016-11-09 16:24:47 -0500

    Add files via upload

---
+++ ZoomToQuery.js
@@ -0,0 +1,20 @@
+// Debes importar las librerias FeatureLayer, Query
+
+var tuLayer = new FeatureLayer( ".../FeatureServer/1", {  
+  		  									mode: FeatureLayer.MODE_SELECTION,  
+  		  									outFields: ["*"]  
+  		  									});
+// Esta funcion la invocas en el evento onClick del boton o combo que dispara la consulta
+function zoomToQuery (){
+	var query = new Query(); 
+	query.where = "tuConsulta ;)";
+	query.returnGeometry = true;
+	tuLayer.selectFeatures(query, FeatureLayer.SELECTION_NEW, function (features) 
+					  {  
+						poly = features[0].geometry;  
+						theExtent = poly.getExtent().expand(2); //Zoom out slightly from the polygon's extent  
+						this.map.setExtent(theExtent);  
+					   }); 
+return;
+
+}
